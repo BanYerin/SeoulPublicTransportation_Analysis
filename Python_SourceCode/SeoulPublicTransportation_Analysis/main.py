28435af0e3f9b969966d132f3227c3aa2a2f9317
BanYerin <xlvl98@gmail.com>
2021-06-08 04:34:09 +0900

    파이썬 소스코드 병합
    *기존
    -시간대별 버스 승차 쿼리 및 시각화
    -시간대별 버스 하차 쿼리 및 시각화
    -시간대별 지하철 승차 쿼리 및 시각화
    -시간대별 지하철 하차 쿼리 및 시각화
    -정류장별 버스 승차 쿼리 및 시각화
    -정류장별 버스 하차 쿼리 및 시각화
    -노선 번호별 버스 승차 쿼리 및 시각화
    -노선 번호별 버스 하차 쿼리 및 시각화
    
    *병합으로 추가된 부분
    -역별 지하철 승차 쿼리 및 시각화
    -역별 지하철 하차 쿼리 및 시각화
    -호선별 지하철 승차 쿼리 및 시각화
    -호선별 지하철 하차 쿼리 및 시각화

--- Python_SourceCode/SeoulPublicTransportation_Analysis/main.py
+++ Python_SourceCode/SeoulPublicTransportation_Analysis/main.py
@@ -1,7 +1,7 @@
 """
-*작성자: 2017038023 반예린
 
-*해당 소스파일 내용
+<작성자 및 담당 부분>
+*2017038023 반예린
 -시간대별 버스 승차 쿼리 및 시각화
 -시간대별 버스 하차 쿼리 및 시각화
 -시간대별 지하철 승차 쿼리 및 시각화
@@ -10,6 +10,12 @@
 -정류장별 버스 하차 쿼리 및 시각화
 -노선 번호별 버스 승차 쿼리 및 시각화
 -노선 번호별 버스 하차 쿼리 및 시각화
+
+*2015023025 배나영
+-역별 지하철 승차 쿼리 및 시각화
+-역별 지하철 하차 쿼리 및 시각화
+-호선별 지하철 승차 쿼리 및 시각화
+-호선별 지하철 하차 쿼리 및 시각화
 
 """
 
@@ -321,7 +327,7 @@ for item in busTimeRideDoc:
     busTimeRideList.append(item['twenty_three_ride_num'])
 
 
-print(busTimeRideList) #배열 내용 확인
+print('시간대별 버스 승차 인원: ', busTimeRideList) #배열 내용 확인
 
 plt.plot(busTimeRideList, color='green', marker='o')
 plt.xticks(np.arange(0, 24)) #x축: 시각(0시~23시)
@@ -364,7 +370,7 @@ for item in busTimeAlightDoc:
     busTimeAlightList.append(item['twenty_three_alight_num'])
 
 
-print(busTimeAlightList) #배열 내용 확인
+print('시간대별 버스 하차 인원: ', busTimeAlightList) #배열 내용 확인
 
 plt.plot(busTimeAlightList, color='green', marker='o')
 plt.xticks(np.arange(0, 24)) #x축: 시각(0시~23시)
@@ -407,7 +413,7 @@ for item in subwayTimeRideDoc:
     subwayTimeRideList.append(item['twenty_three_ride_num'])
 
 
-print(subwayTimeRideList) #배열 내용 확인
+print('시간대별 지하철 승차 인원: ', subwayTimeRideList) #배열 내용 확인
 
 
 plt.plot(subwayTimeRideList, color='green', marker='o')
@@ -451,7 +457,7 @@ for item in subwayTimeAlightDoc:
     subwayTimeAlightList.append(item['twenty_three_alight_num'])
 
 
-print(subwayTimeAlightList) #배열 내용 확인
+print('시간대별 지하철 하차 인원: ', subwayTimeAlightList) #배열 내용 확인
 
 plt.plot(subwayTimeAlightList, color='green', marker='o')
 plt.xticks(np.arange(0, 24)) #x축: 시각(0시~23시)
@@ -474,8 +480,8 @@ for item in busStaRideDoc:
 
 
 #배열 내용 확인
-print(busStaList)
-print(busStaRideList)
+print('top5 버스 정류장 리스트: ', busStaList)
+print('top5 정류장별 버스 승차 인원: ', busStaRideList)
 
 plt.hist(busStaList, bins=None, density=None, weights=busStaRideList, cumulative=False) #x축: 버스 정류장 이름, y축: 버스 승차 인원
 plt.xlabel('버스 정류장 이름')
@@ -497,8 +503,8 @@ for item in busStaAlightDoc:
 
 
 #배열 내용 확인
-print(busStaList)
-print(busStaAlightList)
+print('top5 버스 정류장 리스트: ', busStaList)
+print('top5 정류장별 버스 하차 인원: ', busStaAlightList)
 
 plt.hist(busStaList, bins=None, density=None, weights=busStaAlightList, cumulative=False) #x축: 버스 정류장 이름, y축: 버스 하차 인원
 plt.xlabel('버스 정류장 이름')
@@ -520,8 +526,8 @@ for item in busRouteRideDoc:
 
 
 #배열 내용 확인
-print(busRouteList)
-print(busRouteRideList)
+print('top5 버스 노선번호 리스트: ', busRouteList)
+print('top5 노선 번호별 버스 승차 인원: ', busRouteRideList)
 
 plt.hist(busRouteList, bins=None, density=None, weights=busRouteRideList, cumulative=False) #x축: 버스 노선 번호, y축: 버스 승차 인원
 plt.xlabel('버스 노선 번호')
@@ -543,8 +549,8 @@ for item in busRouteAlightDoc:
 
 
 #배열 내용 확인
-print(busRouteList)
-print(busRouteAlightList)
+print('top5 버스 노선번호 리스트: ', busRouteList)
+print('top5 노선 번호별 버스 하차 인원: ', busRouteAlightList)
 
 plt.hist(busRouteList, bins=None, density=None, weights=busRouteAlightList, cumulative=False) #x축: 버스 노선 번호, y축: 버스 하차 인원
 plt.xlabel('버스 노선 번호')
@@ -552,4 +558,175 @@ plt.ylabel('버스 하차 인원 (단위: 명)')
 plt.title('노선 번호별 버스 하차 인원(인원 많은 상위 5개)')
 plt.show() #결과 그래프 출력
 
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#역이름별 지하철 승차 평균인원 시각화
+sta_avg_ride_pipelines=[{ "$project": {"sub_sta_nm":1,  "four_ride_num":1, "five_ride_num":1, "six_ride_num":1, "seven_ride_num":1, "eight_ride_num":1,
+                                   "nine_ride_num":1, "ten_ride_num":1, "eleven_ride_num":1, "twelve_ride_num":1, "thirteen_ride_num":1, "fourteen_ride_num":1,
+                                   "fifteen_ride_num":1, "sixteen_ride_num":1, "seventeen_ride_num":1, "eighteen_ride_num":1, "nineteen_ride_num":1,
+                                   "twenty_ride_num":1, "twenty_one_ride_num":1, "twenty_two_ride_num":1, "twenty_three_ride_num":1, "midnight_ride_num":1,
+                                   "one_ride_num":1, "two_ride_num":1, "three_ride_num":1} },
+                    { "$addFields": { "totalRide": { "$sum": [ "$four_ride_num", "$five_ride_num", "$six_ride_num", "$seven_ride_num", "$eight_ride_num",
+                                                               "$nine_ride_num", "$ten_ride_num", "$eleven_ride_num", "$twelve_ride_num", "$thirteen_ride_num",
+                                                               "$fourteen_ride_num", "$fifteen_ride_num", "$sixteen_ride_num", "$seventeen_ride_num",
+                                                               "$eighteen_ride_num", "$nineteen_ride_num", "$twenty_ride_num", "$twenty_one_ride_num",
+                                                               "$twenty_two_ride_num", "$twenty_three_ride_num", "$midnight_ride_num", "$one_ride_num",
+                                                               "$two_ride_num", "$three_ride_num" ] } } },
+                    { "$group": { "_id": "$sub_sta_nm", "avgRide": { "$avg": "$totalRide" } } },
+                    { "$sort": { "avgRide":-1} },{ "$limit":5 },{ "$project": { "_id":1, "avgRide":1} }]
+
+avg_ride_results=subwayCol.aggregate(sta_avg_ride_pipelines)
+
+avg_ride_x=list()
+avg_ride_y=list()
+max_val=0
+
+print("역이름별 지하철 승차 평균인원")
+for doc in avg_ride_results:
+    avg_ride_x.append(doc['_id'])
+    avg_ride_y.append(round(doc['avgRide'], 2))
+
+    if max_val<doc['avgRide']:
+        max_val=doc['avgRide']
+
+    print(doc)
+
+a = np.arange(5)
+
+plt.subplot(1,2,1)
+plt.bar(a, avg_ride_y)
+plt.title('역이름별 지하철 승차 평균인원')
+plt.ylabel('지하철 인원 (단위: 1e6명)', fontsize=14)
+plt.xticks(a, avg_ride_x, fontsize=12)
+plt.yticks(fontsize=12)
+
+#역이름별 지하철 하차 평균인원 시각화
+sta_avg_alight_pipelines=[{ "$project": {"sub_sta_nm":1,  "four_alight_num":1, "five_alight_num":1, "six_alight_num":1, "seven_alight_num":1,
+                                     "eight_alight_num":1,"nine_alight_num":1, "ten_alight_num":1, "eleven_alight_num":1, "twelve_alight_num":1,
+                                     "thirteen_alight_num":1, "fourteen_alight_num":1,"fifteen_alight_num":1, "sixteen_alight_num":1,
+                                     "seventeen_alight_num":1, "eighteen_alight_num":1, "nineteen_alight_num":1,"twenty_alight_num":1,
+                                     "twenty_one_alight_num":1, "twenty_two_alight_num":1, "twenty_three_alight_num":1, "midnight_alight_num":1,
+                                     "one_alight_num":1, "two_alight_num":1, "three_alight_num":1} },
+                      { "$addFields": { "totalAlight": { "$sum": [ "$four_alight_num", "$five_alight_num", "$six_alight_num", "$seven_alight_num",
+                                                                   "$eight_alight_num","$nine_alight_num", "$ten_alight_num", "$eleven_alight_num",
+                                                                   "$twelve_alight_num", "$thirteen_alight_num","$fourteen_alight_num", "$fifteen_alight_num",
+                                                                   "$sixteen_alight_num", "$seventeen_alight_num","$eighteen_alight_num", "$nineteen_alight_num",
+                                                                   "$twenty_alight_num", "$twenty_one_alight_num","$twenty_two_alight_num",
+                                                                   "$twenty_three_alight_num", "$midnight_alight_num", "$one_alight_num","$two_alight_num",
+                                                                   "$three_alight_num" ] } } },
+                      { "$group": { "_id": "$sub_sta_nm", "avgAlight": { "$avg": "$totalAlight" } } },
+                      { "$sort": { "avgAlight":-1} },{ "$limit":5 },
+                      { "$project": { "_id":1, "avgAlight":1} }]
+
+
+avg_alight_results=subwayCol.aggregate(sta_avg_alight_pipelines)
+
+avg_alight_x=list()
+avg_alight_y=list()
+
+print("역이름별 지하철 하차 평균인원")
+for doc in avg_alight_results:
+    avg_alight_x.append(doc['_id'])
+    avg_alight_y.append(doc['avgAlight'])
+
+    if max_val<doc['avgAlight']:
+        max_val=doc['avgAlight']+50000
+
+    print(doc)
+
+plt.subplot(1,2,2)
+plt.bar(a, avg_alight_y)
+plt.title('역이름별 지하철 하차 평균인원')
+plt.xticks(a, avg_alight_x, fontsize=12)
+plt.yticks([])
+plt.ylim(0, max_val)
+plt.show()
+
+
+#호선별 지하철 승차 평균인원 시각화
+line_avg_ride_pipelines=[{ "$project": {"line_num":1,  "four_ride_num":1, "five_ride_num":1, "six_ride_num":1, "seven_ride_num":1, "eight_ride_num":1,
+                                   "nine_ride_num":1, "ten_ride_num":1, "eleven_ride_num":1, "twelve_ride_num":1, "thirteen_ride_num":1, "fourteen_ride_num":1,
+                                   "fifteen_ride_num":1, "sixteen_ride_num":1, "seventeen_ride_num":1, "eighteen_ride_num":1, "nineteen_ride_num":1,
+                                   "twenty_ride_num":1, "twenty_one_ride_num":1, "twenty_two_ride_num":1, "twenty_three_ride_num":1, "midnight_ride_num":1,
+                                   "one_ride_num":1, "two_ride_num":1, "three_ride_num":1} },
+                    { "$addFields": { "totalRide": { "$sum": [ "$four_ride_num", "$five_ride_num", "$six_ride_num", "$seven_ride_num", "$eight_ride_num",
+                                                               "$nine_ride_num", "$ten_ride_num", "$eleven_ride_num", "$twelve_ride_num", "$thirteen_ride_num",
+                                                               "$fourteen_ride_num", "$fifteen_ride_num", "$sixteen_ride_num", "$seventeen_ride_num",
+                                                               "$eighteen_ride_num", "$nineteen_ride_num", "$twenty_ride_num", "$twenty_one_ride_num",
+                                                               "$twenty_two_ride_num", "$twenty_three_ride_num", "$midnight_ride_num", "$one_ride_num",
+                                                               "$two_ride_num", "$three_ride_num" ] } } },
+                    { "$group": { "_id": "$line_num", "avgRide": { "$avg": "$totalRide" } } },
+                    { "$sort": { "avgRide":-1} },{ "$limit":5 },{ "$project": { "_id":1, "avgRide":1} }]
+
+line_avg_ride_results=subwayCol.aggregate(line_avg_ride_pipelines)
+
+line_avg_ride_x=list()
+line_avg_ride_y=list()
+max_val=0
+
+print("호선별 지하철 승차 평균인원")
+for doc in line_avg_ride_results:
+    line_avg_ride_x.append(doc['_id'])
+    line_avg_ride_y.append(round(doc['avgRide'], 2))
+
+    if max_val<doc['avgRide']:
+        max_val=doc['avgRide']
+
+    print(doc)
+
+a = np.arange(5)
+
+plt.subplot(1,2,1)
+plt.bar(a, line_avg_ride_y)
+plt.title('호선별 지하철 승차 평균인원')
+plt.ylabel('지하철 인원 (단위: 명)', fontsize=14)
+plt.xticks(a, line_avg_ride_x, fontsize=12)
+plt.yticks(fontsize=12)
+
+#호선별 지하철 하차 평균인원 시각화
+line_avg_alight_pipelines=[{ "$project": {"line_num":1,  "four_alight_num":1, "five_alight_num":1, "six_alight_num":1, "seven_alight_num":1,
+                                     "eight_alight_num":1,"nine_alight_num":1, "ten_alight_num":1, "eleven_alight_num":1, "twelve_alight_num":1,
+                                     "thirteen_alight_num":1, "fourteen_alight_num":1,"fifteen_alight_num":1, "sixteen_alight_num":1,
+                                     "seventeen_alight_num":1, "eighteen_alight_num":1, "nineteen_alight_num":1,"twenty_alight_num":1,
+                                     "twenty_one_alight_num":1, "twenty_two_alight_num":1, "twenty_three_alight_num":1, "midnight_alight_num":1,
+                                     "one_alight_num":1, "two_alight_num":1, "three_alight_num":1} },
+                      { "$addFields": { "totalAlight": { "$sum": [ "$four_alight_num", "$five_alight_num", "$six_alight_num", "$seven_alight_num",
+                                                                   "$eight_alight_num","$nine_alight_num", "$ten_alight_num", "$eleven_alight_num",
+                                                                   "$twelve_alight_num", "$thirteen_alight_num","$fourteen_alight_num", "$fifteen_alight_num",
+                                                                   "$sixteen_alight_num", "$seventeen_alight_num","$eighteen_alight_num", "$nineteen_alight_num",
+                                                                   "$twenty_alight_num", "$twenty_one_alight_num","$twenty_two_alight_num",
+                                                                   "$twenty_three_alight_num", "$midnight_alight_num", "$one_alight_num","$two_alight_num",
+                                                                   "$three_alight_num" ] } } },
+                      { "$group": { "_id": "$line_num", "avgAlight": { "$avg": "$totalAlight" } } },
+                      { "$sort": { "avgAlight":-1} },{ "$limit":5 },
+                      { "$project": { "_id":1, "avgAlight":1} }]
+
+
+line_avg_alight_results=subwayCol.aggregate(line_avg_alight_pipelines)
+
+line_avg_alight_x=list()
+line_avg_alight_y=list()
+
+print("호선별 지하철 하차 평균인원")
+for doc in line_avg_alight_results:
+    line_avg_alight_x.append(doc['_id'])
+    line_avg_alight_y.append(doc['avgAlight'])
+
+    if max_val<doc['avgAlight']:
+        max_val=doc['avgAlight']+50000
+
+    print(doc)
+
+plt.subplot(1,2,2)
+plt.bar(a, line_avg_alight_y)
+plt.title('호선별 지하철 하차 평균인원')
+plt.xticks(a, line_avg_alight_x, fontsize=12)
+plt.yticks([])
+plt.ylim(0, max_val)
+plt.show()
+
+
+myclient.close()
 
